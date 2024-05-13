52de7aa4c2e46c425b847e78691530a74cd3c227
Ramsey Rimkeit <rrimkeit@hotmail.com>
2024-05-13 08:58:59 -0700

    Created insert_statement.sql to practice an insert statement with multiple rows.

---
+++ insert_statement.sql
@@ -0,0 +1,9 @@
+-- be sure to include column names if ID is an auto increment value
+-- be sure to add VALUE keyword after column names
+-- be sure to include commas between each group of values
+
+INSERT INTO Horse (RegisteredName, Breed, Height, BirthDate) VALUES
+('Babe', 'Quarter Horse', 15.3, '2015-02-10'),
+('Independence', 'Holsteiner', 16.0, '2017-03-13'),
+('Ellie', 'Saddlebred', 15.0, '2016-12-22'),
+(NULL, 'Egyptian Arab', 14.9, '2019-10-12');
