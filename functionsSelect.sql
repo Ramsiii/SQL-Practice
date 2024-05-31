68bc7a933dd5cafe4eca9b558a75e552d6322c1f
Ramsey Rimkeit <rrimkeit@hotmail.com>
2024-05-31 13:26:10 -0700

    Added select statement that returns the square root of column X, rounded to the 3 decimal points.

--- functionsSelect.sql
+++ functionsSelect.sql
@@ -10,3 +10,7 @@ FROM Student;
 -- returns the length of time of the class in minutes.
 SELECT TIMEDIFF(EndTime, StartTime)
 FROM ClassSchedule;
+
+-- returns the square root of column X, rounded to the 3 decimal points.
+SELECT ROUND(SQRT(X), 3)
+FROM Book;
