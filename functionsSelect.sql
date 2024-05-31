1c369f69bee03eb1fe227d8db4041f9e9d16bbf9
Ramsey Rimkeit <rrimkeit@hotmail.com>
2024-05-31 13:22:32 -0700

    Added a select statement that returns the length of time of the class in minutes.

--- functionsSelect.sql
+++ functionsSelect.sql
@@ -6,3 +6,7 @@ FROM Book;
 -- returns the student's first name in all caps with no leading or trailing spaces
 SELECT TRIM(UPPER(FirstName))
 FROM Student;
+
+-- returns the length of time of the class in minutes.
+SELECT TIMEDIFF(EndTime, StartTime)
+FROM ClassSchedule;
